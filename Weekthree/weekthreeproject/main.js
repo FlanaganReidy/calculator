19b6b5ebc04356ba05aad3e974d4366b62974a6e
Shannon <reidy.pf@gmail.com>
2017-08-09 17:32:30 -0500

    making sure everything up to date

--- Weekthree/weekthreeproject/main.js
+++ Weekthree/weekthreeproject/main.js
@@ -2,7 +2,7 @@
 
 let buttons = document.querySelectorAll('button'); //getElementsByClassName returns a node list not a list
 let equation = [];
-let newNumber = 0;
+let newNumber = "";
 let operator;
 let output = 0;
 
@@ -10,66 +10,66 @@ buttons.forEach(function(button) {
   button.addEventListener("click", function() {
     switch (button.value) {
       case '+':
-        equation.push(parseInt(newNumber, 10)); //the number we've written is now over and we push it onto our equation array
+        equation.push(parseFloat(newNumber)); //the number we've written is now over and we push it onto our equation array
         newNumber += '+'; //we display the + sign
         operator = '+';
         document.querySelector('output').innerHTML = `<p>${newNumber}</p>`; //turns the output into the new number and the operator
-        newNumber = 0; // resets the new number
+        newNumber = ""; // resets the new number
 
         break;
       case '-':
-        equation.push(parseInt(newNumber, 10));
+        equation.push(parseFloat(newNumber));
         newNumber += '-';
         operator = '-';
         document.querySelector('output').innerHTML = `<p>${newNumber}</p>`;
-        newNumber = 0;
+        newNumber = "";
 
         break;
       case 'x':
-        equation.push(parseInt(newNumber, 10));
+        equation.push(parseFloat(newNumber));
         newNumber += 'x';
         operator = 'x';
         document.querySelector('output').innerHTML = `<p>${newNumber}</p>`;
-        newNumber = 0;
+        newNumber = "";
 
 
         break;
       case '/':
-        equation.push(parseInt(newNumber, 10));
+        equation.push(parseFloat(newNumber));
         newNumber += '/';
         operator = '/';
         document.querySelector('output').innerHTML = `<p>${newNumber}</p>`;
-        newNumber = 0;
+        newNumber = "";
 
 
         break;
       case 'c':
-        newNumber = 0;
+        newNumber = "";
         operator = undefined;
         equation.splice(0, equation.length);
         document.querySelector('output').innerHTML = `<p>${newNumber}</p>`;
 
         break;
       case '=':
-        equation.push(parseInt(newNumber, 10));
+        equation.push(parseFloat(newNumber));
         switch (operator) {
           case '+':
             output = equation[equation.length-1] + equation[equation.length-2];
-            document.querySelector('output').innerHTML = `<p>${output}</p>`;
+            document.querySelector('output').innerHTML = `<p>${cutDecimal(output)}</p>`;
             break;
           case '-':
             output = equation[equation.length-2] - equation[equation.length-1];
-            document.querySelector('output').innerHTML = `<p>${output}</p>`;
+            document.querySelector('output').innerHTML = `<p>${cutDecimal(output)}</p>`;
 
             break;
           case 'x':
             output = equation[equation.length-2] * equation[equation.length-1];
-            document.querySelector('output').innerHTML = `<p>${output}</p>`;
+            document.querySelector('output').innerHTML = `<p>${cutDecimal(output)}</p>`;
 
             break;
           case '/':
             output = equation[equation.length-2] / equation[equation.length-1];
-            document.querySelector('output').innerHTML = `<p>${output}</p>`;
+            document.querySelector('output').innerHTML = `<p>${cutDecimal(output)}</p>`;
 
             break;
 
@@ -79,12 +79,26 @@ buttons.forEach(function(button) {
         }
         break;
       default:
-        newNumber += button.value;
-        newNumber = parseInt(newNumber);
-        document.querySelector('output').innerHTML = `<p>${newNumber}</p>`;
-
+          if (newNumber === "" && parseInt(newNumber) === true ){
+            newNumber = button.value;
+          }
+          else{
+            if(newNumber.length <=  4){
+            newNumber += button.value;
+            }
+          }
+        document.querySelector('output').innerHTML = `<p>${parseFloat(newNumber)}</p>`;
     }
-    // newNumber += button.value;
-    //aNumber.push(parseInt(newNumber, 10));
   });
 });
+
+function cutDecimal(output) {
+  let truncateOutput = output.toString();
+  if (truncateOutput.indexOf('.') !== -1) {
+    let cutHere = truncateOutput.indexOf('.');
+    let newoutput = truncateOutput.substring(0, cutHere + 6);
+    return newoutput;
+  } else {
+    return truncateOutput;
+  }
+}
